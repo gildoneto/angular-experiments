358f0708667d28b223fd3bef226a2f5e4bd8ef31
Gildo Neto <gildo.neto@thoughtworks.com>
2023-03-05 20:41:08 -0300

    Criar componente form-consulta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormConsultaComponent } from './components/form-consulta/form-consulta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormConsultaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/form-consulta/form-consulta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-consulta',
+  templateUrl: './form-consulta.component.html',
+  styleUrls: ['./form-consulta.component.css']
+})
+export class FormConsultaComponent {
+
+}
